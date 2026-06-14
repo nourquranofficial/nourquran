055f2ac49b74a22613dbed4b17cba5d345ae1b28
nourquranofficial <nourquranofficialone@gmail.com>
2026-06-14 13:19:03 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'nour-v1';
+
+const FILES = [
+  '/nourquran/',
+  '/nourquran/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
